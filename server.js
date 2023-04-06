51fab5127477707161a60bbbb20746e096b6a78e
Matt Keane <mattpkeane@gmail.com>
2023-04-06 11:25:38 -0500

    added front end build

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ if (!process.env.PRODUCTION){
 
 const express = require('express');
 const http = require('http');
+const path = require('path');
+const favicon = require('serve-favicon');
 const { Server } = require('socket.io');
 
 require('./util/db');
@@ -12,7 +14,10 @@ const { PORT } = process.env;
 const app = express();
 const server = http.createServer(app);
 
+//  Middleware 
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // socket.io
 const io = new Server(server);
@@ -23,6 +28,10 @@ io.of('/spawn').on('connection', socket => spawnAttemptHandler(io, socket));
 // REST
 const roomController = require('./controllers/roomController');
 app.post('/room/new', roomController);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 server.listen(PORT, () => {
     const d = new Date();
